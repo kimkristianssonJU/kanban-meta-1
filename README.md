667b63961805b105b4a1e21d19f38f64676aca6f
kimkristianssonJU <krki21cn@student.ju.se>
2021-10-18 15:04:47 +0200

    Rättat fel i JSON-filerna, uppdaterat README.md, omorganiserat filerna till mappar

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# kanban-meta-1
-En kanban-app
+#  README - Kanban Meta 1
+
+## Struktur och organisering av filer
+kanban-meta-1/
+
+├── style/
+│⠀⠀⠀└── _<Alla CSS-filer>_
+│⠀⠀⠀│
+│⠀⠀⠀└── _modules/_
+│⠀⠀⠀⠀⠀⠀⠀└── _<Alla module-filer>_
+│
+├── js/
+│⠀⠀⠀└── _<Alla JavaScript-filer>_
+│
+├── json/
+│⠀⠀⠀└── _<Alla JSON-filer>_
+│ 
+└──  _<Alla HTML-filer>_
+
+## Regler i namngivning och språk
+
+### Språk
+* _Variabler_, _funktioner_, _filer_, _objekt_ och _klasser_ ska döpas med __engelska__ ord för att stämma in med JavaScript och undvika svenska bokstäver.
+* Vi skriver kommentarer på __svenska__. Vi är medvetna om målgruppen för projektet, och vi förvänta oss inte att intresserade utanför sfären JU kommer att ta del av källkoden och vi kan därför tillåtas avvika från engelska.
+
+### Namngivning och konventioner
+* Funtioner döps med _camelCase_: `myFunction();`
+* Variabler döps med _camelCase_: `myVariable;`
+* Objekter döps med _camelCase_: `myObject;`
+* Klass döps med _PascalCase_: `MyClass;`
+* HTML id och klasser döps med _dash-case_: `my-html-class`
+* Filer döps med _snake_case_: `my_file.js`
+
+## Referenser till källor och andra länkar
+**YouTube**
+[How To Build Sortable Drag & Drop With Vanilla Javascript](https://www.youtube.com/watch?v=jfYWwQrtzzY&t=9s)
+[Kom igång med moduler i Javascript](https://www.youtube.com/watch?v=tzl00lcD1Os&t=450s)
+
+**GitHub**
+[GitHub - Kanban Meta 1](https://github.com/kimkristianssonJU/kanban-meta-1)
+
+**MDN Web Docs - Mozilla**
+[Klasser- JavaScript](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes?retiredLocale=sv-SE)
+[Objekt- JavaScript](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object)
---
+++ js/script.js
@@ -0,0 +1,82 @@
+
+// hämta id från index.html
+let userNameInput = document.getElementById('username');
+let passwordInput = document.getElementById('password');
+let logInButton = document.getElementById('btnInlogg');
+
+let div1 = document.getElementById("div1");
+let div2 = document.getElementById("div2");
+// inloggningsida ska inte visas
+div2.style.display = "none";
+
+divWrong = printOutWrong();
+stayInlog();
+
+
+logInButton.addEventListener("click", function(){
+
+    fetch("./json/users.json")
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(json){
+        console.log(json);
+        for(i=0; i<json.length; i++)
+        {
+            if(userNameInput.value === json[i].name && passwordInput.value === json[i].pass)
+            {
+                div1.style.display = "none";
+                div2.style.display = "initial";
+                localStorage.setItem("userId", i);
+                location.reload(); // för att log out button ska visas
+            }else{
+                divWrong.style.display = "initial";
+            };
+        }
+    })
+});
+
+// en funktion till att visa meddelande om användare har skrivit fel user eller pass
+function printOutWrong(){
+    let div = document.createElement('div');
+    div.id = "LogInPage2";
+    document.body.appendChild(div);
+    
+    let header = document.createElement("h1");
+    header.innerHTML = "Wrong user or password";
+    div.appendChild(header);
+
+    div.style.display = "none";
+    return div;  
+}
+
+// skapa en log out button
+function logOutForm(){
+    let logOutBtn = document.createElement("button")
+    logOutBtn.innerText = "Log out";
+    logOutBtn.id = "logOut";
+    div2.appendChild(logOutBtn);
+}
+// användaren ska vara inloggad om hen har redan skrivit rätt user och pass
+function stayInlog(){
+    let l = localStorage.getItem("userId");
+    if (l !== null){
+        div1.style.display = "none";
+        div2.style.display = "initial";
+        logOutForm();
+        logOut();
+    }
+    else{
+        div1.style.display = "initial";
+        div2.style.display = "none";
+    }
+}
+// rensa localStorage när användaren klickar på log out button
+function logOut(){
+    let logOutBtn = document.getElementById("logOut");
+    logOutBtn.addEventListener("click", function(){
+        localStorage.clear();
+        location.reload();
+
+    })
+}
