e81abd1dc7233024ae73199772b0aa3877fb5b59
kimkristianssonJU <krki21cn@student.ju.se>
2021-10-20 15:32:55 +0200

    inloggning funkar

--- js/main.js
+++ js/main.js
@@ -1,24 +1,45 @@
 // Modules imports!
 import { fetchThis } from "./modules/fetch_function.mjs"; // Fetch
 import { dragAndDrop } from "./modules/drag_and_drop.mjs";
-import { addCard } from "./modules/add_card.mjs";
 import { addButtonFunc } from "./modules/button_add_card.mjs";
 
 // Adresser till JSON-filerna
 const taskUrl = '../json/task.json';
 const usersUrl = '../json/users.json';
 
-/*
+const verificationKeyUser = "isVerfied";
+
+
 async function mainFunction() {
     const tasks = await fetchThis(taskUrl);
-    // login
-    if(true)
-    // f
-    else 
-    // retry
-    console.log(tasks[0].id); // Returnerar "1"
+    const users = await fetchThis(usersUrl);
+    let success = false;
+
+    if (!localStorage.getItem(verificationKeyUser)) {
+
+        let logInBtn = document.getElementById("login-button");
+
+        logInBtn.addEventListener("click", () => {
+            const userInput = document.getElementById("user-name");
+            const passInput = document.getElementById("password");
+            let message = document.getElementById("message");
+
+            for (const user of users) {
+                if (user.name.toLowerCase() === userInput.value.toLowerCase() && user.pass === passInput.value) {
+                    // Användaren inloggad
+                    location.replace("kanban.html");
+                }
+            }
+            if (!success) {
+                message.innerHTML = "Incorrect user name or password";
+            }
+        });
+    }
+    else {
+        // Gå till kanban
+    }
 }
-*/
+
 
 addButtonFunc();
 dragAndDrop();
