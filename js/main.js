c8c1fed75117d6a654f5e8860a1c41bad2658b45
Emil Aronsson <aronsson.emil@hotmail.se>
2021-10-20 11:24:23 +0200

    Uppdaterat HTML och lagt till drag and drop.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,25 @@
 // Modules imports!
 import { fetchThis } from "./modules/fetch_function.mjs"; // Fetch
+import { dragAndDrop } from "./modules/drag_and_drop.mjs";
+import { addCard } from "./modules/add_card.mjs";
+import { addButtonFunc } from "./modules/button_add_card.mjs";
 
 // Adresser till JSON-filerna
 const taskUrl = '../json/task.json';
 const usersUrl = '../json/users.json';
 
-
+/*
 async function mainFunction() {
     const tasks = await fetchThis(taskUrl);
+    // login
+    if(true)
+    // f
+    else 
+    // retry
     console.log(tasks[0].id); // Returnerar "1"
 }
+*/
 
-mainFunction()
+addButtonFunc();
+dragAndDrop();
+mainFunction();
---
+++ js/modules/add_card.mjs
@@ -0,0 +1,15 @@
+// customeCreateElement("html-tag", "attribute<id, klass>", "klassnamn", parent)
+import { customCreateElement } from "./create_element.mjs";
+import { dragAndDrop } from "./drag_and_drop.mjs";
+
+export function addCard(parent, textarea) { // Parent: .drag-list-container
+    if (textarea === '') {
+        alert("You must write something!");
+    } else {
+        const draggable = customCreateElement("div", "class", "draggable", parent);
+        draggable.setAttribute("draggable", "true");
+        const p = customCreateElement("p", "", "", draggable);
+        p.textContent = textarea.value;
+        dragAndDrop();
+    }
+}
